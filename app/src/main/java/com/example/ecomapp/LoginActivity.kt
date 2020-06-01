6370515b241e52152639bacfd86bf7c32a984efc
Pramesh <pramesh@techsevin.com>
2020-06-01 17:53:46 +0530

    Login screen created

--- app/src/main/java/com/example/ecomapp/LoginActivity.kt
+++ app/src/main/java/com/example/ecomapp/LoginActivity.kt
@@ -1,12 +1,30 @@
 package com.example.ecomapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.ecomapp.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityLoginBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        binding.loginButton.setOnClickListener(View.OnClickListener {
+
+            startActivity(Intent(this,MainActivity :: class.java))
+            finish()
+
+        })
+
+
+
     }
 }
